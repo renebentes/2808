81a6f09a0f08c92ca1d333113231f0b201d763da
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-08-16 21:12:46 -0300

    feat: Remove Tags no banco

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -4,8 +4,6 @@ using Blog.Models;
 using var context = new BlogDataContext();
 
 var tag = context.Tags.FirstOrDefault(tag => tag.Id == 2);
-tag.Name = ".NET";
-tag.Slug = "dotnet";
 
-context.Update(tag);
+context.Remove(tag);
 context.SaveChanges();
