68d485dc1be5062e5b04befcdbae394754a8cec6
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-09-21 16:29:12 -0300

    feat: Cria post, categoria e autor no banco de dados
    O uso do EF facilita a criação de dados relacionados,
    com uma única instrução de adição, no conjunto principal,
    todos os itens relacionados são persistidos no banco e,
    ainda, gerenciando transação de forma automatizada.

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,19 +1,32 @@
 using Blog.Data;
-using Microsoft.EntityFrameworkCore;
+using Blog.Models;
 
 using var context = new BlogDataContext();
 
-var tag = context
-    .Tags
-    .AsNoTracking()
-    // First() - Retorna primeiro elemento ou exception quando condição não
-    // satisfeita
-    // FirstOrDefault() - Retorna primeiro elemento ou null quando condição
-    // não satisfeita
-    // Single() - Retorna único elemento ou exceção quando condição não
-    // satisfeita ou mais de um
-    // SingleOrDefault() - Retorna único elemento ou null quando condição
-    // não satisfeita ou exception quando mais elements satisfazem a condição
-    .SingleOrDefault(it => it.Id == 3);
+var user = new User
+{
+    Name = "André Baltieri",
+    Slug = "andrebaltieri",
+    Email = "andre@balta.io",
+    Bio = "9x Microsoft MVP",
+    Image = "https://balta.io",
+    PasswordHash = "123098457"
+};
 
-Console.WriteLine(tag?.Name);
+var category = new Category { Title = "Backend", Slug = "backend" };
+
+var post = new Post
+{
+    Author = user,
+    Category = category,
+    Body = "<p>Hello world</p>",
+    Slug = "comecando-com-ef-core",
+    Summary = "Neste artigo vamos aprender EF core",
+    Title = "Começando com EF Core",
+    CreateDate = DateTime.Now,
+    LastUpdateDate = DateTime.Now,
+};
+
+context.Posts.Add(post);
+context.SaveChanges();
+
