a3504cabc5c392dcfd6c4a90078a88b20c021bc7
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-09-21 14:01:01 -0300

    feat: Desabilita rastreamento de objetos do EF

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,12 +1,13 @@
 using Blog.Data;
+using Microsoft.EntityFrameworkCore;
 
 using var context = new BlogDataContext();
 
 var tags = context
     .Tags
-    // Filtro antes da execução
-    .Where(it => it.Name.Contains(".NET"))
-    // Executa a query no banco
+    // Desabilita metadados de traqueamento (referências entre os objetos)
+    // Usar somente em modo de leitura, nunca com operações de escrita
+    .AsNoTracking()
     .ToList();
 
 foreach (var tag in tags)
