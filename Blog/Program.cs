d73eb1eec557eb33f36417eeb83acdc37d9d457c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-09-21 16:49:24 -0300

    feat: Imprime posts e seus autores ordenados por data
    O JOIN só é aplicado quando utilizado o método `Include()`,
    é uma boa prática sempre usar o operador de null safe `?`
    em objetos relacionados.

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,32 +1,19 @@
 using Blog.Data;
-using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 using var context = new BlogDataContext();
 
-var user = new User
+var posts = context
+    .Posts
+    .AsNoTracking()
+    // Efetua o JOIN
+    .Include(p => p.Author)
+    .OrderByDescending(p => p.LastUpdateDate)
+    .ToList();
+
+foreach (var post in posts)
 {
-    Name = "André Baltieri",
-    Slug = "andrebaltieri",
-    Email = "andre@balta.io",
-    Bio = "9x Microsoft MVP",
-    Image = "https://balta.io",
-    PasswordHash = "123098457"
-};
+    Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+}
 
-var category = new Category { Title = "Backend", Slug = "backend" };
-
-var post = new Post
-{
-    Author = user,
-    Category = category,
-    Body = "<p>Hello world</p>",
-    Slug = "comecando-com-ef-core",
-    Summary = "Neste artigo vamos aprender EF core",
-    Title = "Começando com EF Core",
-    CreateDate = DateTime.Now,
-    LastUpdateDate = DateTime.Now,
-};
-
-context.Posts.Add(post);
-context.SaveChanges();
 
