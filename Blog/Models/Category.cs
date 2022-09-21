156920c8fa6ea1ee8e89165e2f65d708a4a23fbc
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-09-21 15:56:24 -0300

    feat: Aplica mapeamento das colunas do modelo de categorias
    Define o comportamento para criação da tabela de categorias
    no banco de dados (tipos das colunas, tamanho do campo).

--- Blog/Models/Category.cs
+++ Blog/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models;
@@ -5,7 +6,15 @@ namespace Blog.Models;
 [Table(nameof(Category))]
 public class Category : ModelBase
 {
+    [Required]
+    [MinLength(3)]
+    [MaxLength(80)]
+    [Column(nameof(Slug), TypeName = "VARCHAR")]
     public string Slug { get; set; }
 
+    [Required]
+    [MinLength(3)]
+    [MaxLength(80)]
+    [Column(nameof(Title), TypeName = "NVARCHAR")]
     public string Title { get; set; }
 }
