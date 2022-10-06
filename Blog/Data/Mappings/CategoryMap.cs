45eb6d7b6b4d9d236132250bee497b19bfed00fc
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-10-06 20:33:36 -0300

    refactor: Define a chave primária da categoria

--- Blog/Data/Mappings/CategoryMap.cs
+++ Blog/Data/Mappings/CategoryMap.cs
@@ -9,5 +9,14 @@ public class CategoryMap : IEntityTypeConfiguration<Category>
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.ToTable(nameof(Category));
+
+        // Define a chave primária
+        builder.HasKey(c => c.Id);
+
+        // Define que será usado IDENTITY(1, 1) para geração
+        // automática da chave
+        builder.Property(c => c.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
     }
 }
